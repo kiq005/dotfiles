48e288e286d74ebe270f8fac92dc747f13ff48ad
Kaique de Queiroz <kaique_q@live.com>
2019-09-01 09:36:33 -0300

    Fix recurring next task

--- scripts/next_task.sh
+++ scripts/next_task.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # Find the task with greatest priority on taskwarior
-MSG=$(task _get $(task next limit:1 +READY | tail -n +4 | head -n 1 | sed 's/^ //' | cut -d ' ' -f1).description)
+TASK="$(task next limit:1 +READY '(+DUE or -SCHEDULED)' \
+	| tail -n +4 \
+	| head -n 1 \
+	| sed 's/^ //' \
+	| cut -d ' ' -f1 )"
+MSG="$(task _get "$TASK".description)"
 echo "$MSG"
